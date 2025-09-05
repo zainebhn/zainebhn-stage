6a89d74bdfa7d2f1e0bfe26bd5471a836b548643
zainebhn <heni.zeineb@gmail.com>
2025-09-05 15:26:44 +0100

    Fix Cypress

--- frontend/cypress/integration/search-courses.spec.js
+++ frontend/cypress/integration/search-courses.spec.js
@@ -1,34 +1,46 @@
 describe("Search courses", () => {
+    
+    const expected_courses = [
+        {
+            title: "The Rust Programming Language",
+            upvotes: "6",
+            topic: "Rust"
+        },
+        {
+            title: "Intro to the Rust programming language",
+            upvotes: "5",
+            topic: "Rust"
+        }
+    ];
+
     it("Load search page", () => {
         cy.visit("http://localhost:3000");
-        cy.wait(5000);
+        // Wait for page to load (optional)
+        cy.get("#rc_select_0", { timeout: 10000 }).should("be.visible");
     });
     
     it("Type the course", () => {
-        cy.get("#rc_select_0").type("rust").type("{enter}");
-        cy.wait(5000);
+        cy.get("#rc_select_0").type("rust{enter}");
+        // Wait for results to appear
+        cy.get(".ant-list-item", { timeout: 10000 }).should("have.length.greaterThan", 0);
     });
      
-    it("Compare search result", () => {
-        cy.wait(10000);
-        let expected_courses = [
-                        {
-                            "title": "The Rust Programming Language",
-                            "upvotes": "6",
-                            "topic": "Rust"
-                        },
-                        {
-                            "title": "Intro to the Rust programming language",
-                            "upvotes": "5",
-                            "topic": "Rust"
-                        }
-        ];
-        for (var index=0; index < expected_courses.length; index++) {   
-            const index_pos = index;
-            cy.get(`:nth-child(${index_pos+1}) > .ant-list-item-meta > .ant-list-item-meta-content > .ant-list-item-meta-title > a`).then(($title_elem) => {
-            }).contains("Rust");
-            cy.get(`:nth-child(${index_pos+1}) > .ant-list-item-action > :nth-child(2) > .ant-tag > :nth-child(2)`).then(($topic_elem) => {
-            }).contains("Rust");
-        }
+    it("Compare search results", () => {
+        // Loop through expected courses
+        expected_courses.forEach((course, index) => {
+            cy.get(".ant-list-item")   // get all list items
+              .eq(index)               // pick the item at position index
+              .within(() => {
+                  // Check the title
+                  cy.get(".ant-list-item-meta-title > a")
+                    .should("contain.text", course.title);
+
+                  // Check the topic
+                  cy.get(".ant-tag > span")
+                    .should("contain.text", course.topic);
+              });
+        });
     });
-})
+
+});
+
